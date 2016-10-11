d2a18b3118a24a72fee9ab8e9cd8b38430fa2602
Shelley Wong <swong26@mail.csuchico.edu>
2016-10-10 17:23:46 -0700

    validate that password is not 'password'

--- form-validation.js
+++ form-validation.js
@@ -8,6 +8,7 @@ function validate(){
   console.log(name_length);
   if(name_length >= 6)
   {
+    //name is valid; turn username items green
     document.getElementById("usernameGroup").classList.add("has-success");
   }
   else
@@ -20,11 +21,19 @@ function validate(){
     document.getElementById("usernameGroup").classList.add("has-error");
   }
 
-
-  //Show message that there is an error with the password...
-  document.getElementById("passwordError").innerHTML="Bad password.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
+  //check that the password entered is not "password"
+  if(passEntered == "password")
+  {
+    //Show message that there is an error with the password...
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else
+  {
+    //password is valid; turn password green
+    document.getElementById("passwordGroup").classList.add("has-success");
+  }
 }
