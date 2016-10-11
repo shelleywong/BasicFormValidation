d30320ea314d5274f2356d03b462533cc4ad5c10
Shelley Wong <swong26@mail.csuchico.edu>
2016-10-10 17:11:33 -0700

    validate that username is at least 6 characters long

--- form-validation.js
+++ form-validation.js
@@ -3,12 +3,23 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
-  //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
+  //check length of username (must be at least 6 characters long)
+  var name_length = document.getElementById("user").innerHTML = userEntered.length;
+  console.log(name_length);
+  if(name_length >= 6)
+  {
+    document.getElementById("usernameGroup").classList.add("has-success");
+  }
+  else
+  {
+    //Show message that there is an error with the username...
+    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
+
 
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
